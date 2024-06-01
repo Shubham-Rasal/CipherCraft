d6cb63768a5e32add24c4cd132c4df41d442c2d8
Shubham-Rasal <bluequbits@gmail.com>
2024-06-01 16:48:58 +0530

    add wagmi rainbowkit

---
+++ frontend/src/app/providers.tsx
@@ -0,0 +1,23 @@
+"use client";
+import "@rainbow-me/rainbowkit/styles.css";
+import { getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { WagmiProvider } from "wagmi";
+import { mainnet, polygon, optimism, arbitrum, base } from "wagmi/chains";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+
+const config = getDefaultConfig({
+  appName: "My RainbowKit App",
+  projectId: "YOUR_PROJECT_ID",
+  chains: [mainnet, polygon, optimism, arbitrum, base],
+  ssr: true, // If your dApp uses server side rendering (SSR)
+});
+
+const queryClient = new QueryClient();
+
+export const Web3Provider = ({ children }: any) => (
+  <WagmiProvider config={config}>
+    <QueryClientProvider client={queryClient}>
+      <RainbowKitProvider>{children}</RainbowKitProvider>
+    </QueryClientProvider>
+  </WagmiProvider>
+);
