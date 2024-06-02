46d0ceb97d182640050ccdd61c4b1232daf8d9c9
Ayush <99096397+ayush4345@users.noreply.github.com>
2024-06-02 18:49:16 +0530

    add lit and ipfs

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,12 @@
 import { ConnectButton } from "@rainbow-me/rainbowkit";
+import Hero from "@/components/hero"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <ConnectButton />
+    <main className="flex h-screen flex-col items-center justify-between">
+      {/* <ConnectButton /> */}
+      <Hero />
     </main>
   );
 }
+
