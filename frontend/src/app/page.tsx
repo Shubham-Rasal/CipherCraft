5702ca15bbc8095febef2500109a04d71475d862
Ayush <99096397+ayush4345@users.noreply.github.com>
2024-06-02 19:04:55 +0530

    add feature

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,14 @@
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import Hero from "@/components/hero"
+import Feature from "@/components/feature"
+
 
 export default function Home() {
   return (
     <main className="flex h-screen flex-col items-center justify-between">
       {/* <ConnectButton /> */}
       <Hero />
+      <Feature />
     </main>
   );
 }
